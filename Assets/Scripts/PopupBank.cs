3cc06d4882a9577cbccc535c2dd1040cb5a44339
슬기 <jsg8369@naver.com>
2025-08-28 10:41:38 +0900

    Step9

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupBank : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Btn;
+    public GameObject Deposit;
+    public GameObject Withdraw;
+    public UIFormat uiFormat;
+    public InputField inputDeposit;
+    public InputField inputWithdraw;
+    public Image PopupError;
+    public void OpenDeposit()
     {
-        
+        Deposit.SetActive(true);
+        Withdraw.SetActive(false);
+        Btn.SetActive(false);
+    }
+    
+    public void OpenWithdraw()
+    {
+        Deposit.SetActive(false);
+        Withdraw.SetActive(true);
+        Btn.SetActive(false);
+    }
+    
+    public void Close()
+    {
+        Deposit.SetActive(false);
+        Withdraw.SetActive(false);
+        Btn.SetActive(true); 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DepositBtn(int amount)
     {
-        
+        if (GameManager.instance.userData.Cash < amount)
+        {
+            PopupError.gameObject.SetActive(true);
+            return;
+        }
+        GameManager.instance.userData.Cash -= amount;
+        GameManager.instance.userData.Balance += amount;
+        uiFormat.Refresh();
+    }
+    
+    public void WithdrawBtn(int amount)
+    {
+        if (GameManager.instance.userData.Balance < amount)
+        {
+            PopupError.gameObject.SetActive(true);
+            return;
+        }
+        GameManager.instance.userData.Cash += amount;
+        GameManager.instance.userData.Balance -= amount;
+        uiFormat.Refresh();
+    }
+
+    public void InputDepositBtn()
+    {
+        int amount = int.Parse(inputDeposit.text);
+        if (GameManager.instance.userData.Cash < amount)
+        {
+            PopupError.gameObject.SetActive(true);
+            return;
+        }
+        GameManager.instance.userData.Cash -= amount;
+        GameManager.instance.userData.Balance += amount;
+        uiFormat.Refresh();
+    }
+    
+    public void InputWithdrawBtn()
+    {
+        int amount = int.Parse(inputWithdraw.text);
+        if (GameManager.instance.userData.Balance < amount)
+        {
+            PopupError.gameObject.SetActive(true);
+            return;
+        }
+        GameManager.instance.userData.Cash += amount;
+        GameManager.instance.userData.Balance -= amount;
+        uiFormat.Refresh();
+    }
+    
+    public void ClosePopupError()
+    {
+        PopupError.gameObject.SetActive(false);
     }
 }
